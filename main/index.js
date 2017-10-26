42aeef30d81449cb645fa188235a6efc5fa7ec89
Alexander <lss1ash@rambler.ru>
2017-10-27 01:13:39 +0300

    Не удержался... спёр

--- main/index.js
+++ main/index.js
@@ -20,3 +20,65 @@
 //
 // Nyan cat lies here...
 //
+
+let ws = null;
+let game = null;
+
+App.onToken = (token) => {
+	ws = new WebSocket(`ws://ws.rudenko.tech/life/api?token=${token}`);
+	addHandlers(ws);
+};
+
+function addHandlers(ws) {
+	ws.onopen = e => {
+		console.log(e, 'open');
+	};
+
+	ws.onmessage = e => {
+		const data = JSON.parse(e.data);
+		processResp(data);
+	};
+
+	ws.onerror = e => {
+		console.log(`Error occured: ${e}`);
+	};
+
+	ws.onclose = e => {
+		const wasClean = e.wasClean;
+		console.log(`closing was clean: ${wasClean}`);
+	};
+}
+
+function processResp({type, data}) {
+	switch (type) {
+		case 'INITIALIZE':
+			initGame(data);
+			break;
+		case 'UPDATE_STATE':
+			update(data);
+			break;
+		default:
+			console.log(`Undefined message type - ${type} with data - ${data}`);
+			break;
+	}
+}
+
+function initGame({state, settings, user}) {
+	game = new LifeGame(user, settings);
+	game.init();
+	game.setState(state);
+	game.send = send;
+}
+
+function update(data) {
+	game.setState(data);
+}
+
+function send(data) {
+	const result = JSON.stringify({
+		type: 'ADD_POINT',
+		data
+	});
+
+	ws.send(result);
+}
